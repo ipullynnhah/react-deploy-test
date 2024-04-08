eb5f40c66d6cfd9da5a4c5900e803a31399c5b36
ipullynnhah <ipullynnhah@gmail.com>
2024-04-07 23:15:44 -0300

    projeto test

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./app.css";
+
+function App() {
+  return (
+    <main>
+      <h1>React Deploy Test</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at
+        doloribus eveniet explicabo fugit itaque iusto labore maxime nam nemo
+        nisi odit, officia pariatur quia quo sapiente soluta temporibus ut?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur
+        eum maiores nulla quis rem tempora veniam? A accusantium adipisci
+        dolore, dolorum fugiat, hic ipsum, nobis optio quaerat repudiandae
+        sapiente sed!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
+        consectetur dolores dolorum eligendi eos et eveniet, illum, maxime minus
+        nemo nisi quia quos reiciendis saepe soluta tenetur ullam! Autem, dolor.
+      </p>
+    </main>
+  );
+}
+
+export default App;
